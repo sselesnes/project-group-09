4d00936fc5258eaf33cf283f6be2c61061360fa5
Yuri Boblak <160606809+sselesnes@users.noreply.github.com>
2025-04-29 07:07:55 +0300

    temp4.1

--- src/js/faq.js
+++
@@ -1,8 +0,0 @@
-import Accordion from 'accordion-js';
-import 'accordion-js/dist/accordion.min.css';
-
-new Accordion('.accordion-container', {
-  duration: 300,
-  showMultiple: false,
-  openOnInit: [0],
-});
